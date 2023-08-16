0df32b16a53efa3de1b4ec56bfd9b469430cf005
Eduardo Cintra <educin15@gmail.com>
2023-08-16 07:59:59 -0300

    mudancas no ocelot criacao de um arquivo para configs do ocelot ajuste em bug ao aplicar cupom

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -5,10 +5,12 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 
 app.Run();
